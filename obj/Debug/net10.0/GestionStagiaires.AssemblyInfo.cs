54495eb526d06ad8a5631f8e7a4cc1030f3ad93d
Aymane Dikri <aymanedikri005@gmail.com>
2026-07-23 10:11:12 +0100

    ajout logo onee

--- obj/Debug/net10.0/GestionStagiaires.AssemblyInfo.cs
+++ obj/Debug/net10.0/GestionStagiaires.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionStagiaires")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fdcf0ea5920021f25982ad3d34ac125db5db10d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba7dd23e7e88333212f0ffd87493ef353049b31b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionStagiaires")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionStagiaires")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
