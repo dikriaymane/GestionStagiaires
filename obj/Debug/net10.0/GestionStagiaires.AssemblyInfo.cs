81be8fbd21b5aa850107be05ee40b5816d62e45f
Aymane Dikri <aymanedikri005@gmail.com>
2026-07-29 23:24:36 +0100

    modification logo

--- obj/Debug/net10.0/GestionStagiaires.AssemblyInfo.cs
+++ obj/Debug/net10.0/GestionStagiaires.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionStagiaires")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+788ca995d74455e12b1115d1e3c4d78a624d5120")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64878ee82eb60efcb651400a5de5accc86a58316")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionStagiaires")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionStagiaires")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
